7bb3a3d3134b3ceda07ee6852ee3dbae0a4a7362
JordyTheCoder <carrolljordan449@gmail.com>
2022-01-09 20:23:18 -0500

    Added the C# Solution

---
+++ C#/Program.cs
@@ -0,0 +1,36 @@
+﻿class DeadFish 
+{
+    static void Main(string[] args) 
+    {
+        string file = args[0];
+        if(File.Exists(file)) 
+        {
+            int accumulator = 0;
+            string code = File.ReadAllText(file);
+            for(int i = 0; i < code.Length; i++)
+            {
+                switch(code[i])
+                {
+                    case 'i':
+                        accumulator++;
+                        break;
+                    case 'd':
+                        accumulator--;
+                        break;
+                    case 's':
+                        accumulator *= accumulator;
+                        break;
+                    case 'o':
+                        Console.Write($"{accumulator} ");
+                        break;
+                }
+            }
+
+        }
+        else 
+        {
+            Console.WriteLine("File was not found");
+        }
+
+    }
+}
